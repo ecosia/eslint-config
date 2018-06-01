7be280693fb968027d3434e6ed6235d84d5afa4c
Emanuele Marchi <emanuele.marchi@ecosia.org>
2018-06-01 11:27:31 +0200

    Yo dawg, i heard you like linting
    So i put some linting your linting with a linting test so you can lint while you test the linting

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = {
-  extends: 'airbnb-base',
-  env: {
-    node: true,
-    browser: true,
-    es6: true
-  },
-  parserOptions: {
-    ecmaVersion: 2018
-  },
-  rules: {
+    extends: 'airbnb-base',
+    env: {
+        node: true,
+        browser: true,
+        es6: true
+    },
+    parserOptions: {
+        ecmaVersion: 2018
+    },
+    rules: {
     // comma-dangle would cost us another babel-transform so we skip it for now
-    'comma-dangle': 0,
-    indent: [2, 4]
-  }
+        'comma-dangle': 0,
+        indent: [2, 4]
+    }
 };
--- vue.js
+++ vue.js
@@ -1,7 +1,7 @@
 module.exports = {
-  extends: ['plugin:vue/recommended', './'],
-  rules: {
-    'vue/html-indent': [2, 4],
-    'vue/max-attributes-per-line': [2, {'singleline': 3}]
-  }
-}
+    extends: ['plugin:vue/recommended', './index.js'],
+    rules: {
+        'vue/html-indent': [2, 4],
+        'vue/max-attributes-per-line': [2, { singleline: 3 }]
+    }
+};
