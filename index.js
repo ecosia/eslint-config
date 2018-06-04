4e977b864fbb74b483af6f2739d0fb0cb1bb619f
Emanuele Marchi <emanuele.marchi@ecosia.org>
2018-06-04 17:32:28 +0200

    Set first ruleset

--- index.js
+++ index.js
@@ -9,8 +9,17 @@ module.exports = {
         ecmaVersion: 2018
     },
     rules: {
-    // comma-dangle would cost us another babel-transform so we skip it for now
-        'comma-dangle': 0,
-        indent: [2, 4]
+        // comma-dangle would cost us another babel-transform so we skip it for now
+        'comma-dangle': [2, { functions: 'never' }],
+        'function-paren-newline': [0],
+        'import/no-extraneous-dependencies': [2, { devDependencies: true }],
+        'indent': [2, 4],
+        'no-alert': 2,
+        'no-console': 1,
+        'no-new': 0,
+        'no-restricted-globals': [2, 'event'],
+        'object-curly-newline': 0,
+        'quote-props': [2, 'consistent-as-needed'],
+        'space-before-function-paren': 1
     }
 };
