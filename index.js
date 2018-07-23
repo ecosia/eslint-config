ddce8fe2156aacc28ddfcc8a6a04ba685c0cf2e0
Emanuele Marchi <emanuele.marchi@ecosia.org>
2018-07-23 16:16:04 +0200

    Change indentation from 4 to 2

--- index.js
+++ index.js
@@ -16,27 +16,27 @@
  */
 
 module.exports = {
-    extends: 'airbnb-base',
-    env: {
-        node: true,
-        browser: true,
-        es6: true,
-    },
-    parserOptions: {
-        ecmaVersion: 2018,
-    },
-    rules: {
-        // comma-dangle would cost us another babel-transform so we skip it for now
-        'comma-dangle': ['error', 'always-multiline'],
-        'function-paren-newline': ['off'],
-        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-        'indent': ['error', 4],
-        'no-alert': 'error',
-        'no-console': 'warn',
-        'no-new': 'off',
-        'no-restricted-globals': ['error', 'event'],
-        'object-curly-newline': 'off',
-        'quote-props': ['error', 'consistent-as-needed'],
-        'space-before-function-paren': 'warn',
-    },
+  extends: 'airbnb-base',
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    // comma-dangle would cost us another babel-transform so we skip it for now
+    'comma-dangle': ['error', 'always-multiline'],
+    'function-paren-newline': ['off'],
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'indent': ['error', 2],
+    'no-alert': 'error',
+    'no-console': 'warn',
+    'no-new': 'off',
+    'no-restricted-globals': ['error', 'event'],
+    'object-curly-newline': 'off',
+    'quote-props': ['error', 'consistent-as-needed'],
+    'space-before-function-paren': 'warn',
+  },
 };
